53a461b671c0e984eb28628c1fe8198fdf0db509
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-30 21:23:59 -0300

    feat: create login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,29 @@
+import { signIn } from 'next-auth/react'
+import styles from '../styles/pages/Login.module.css'
+
+export default function Login() {
+  return (
+    <div className={styles.container}>
+      <div>
+        <img src="assets/background-login.svg" alt="" />
+
+        <main>
+          <img src="assets/logo.svg" alt="" />
+
+          <h1>Bem-vindo</h1>
+          <button
+            type="button"
+            onClick={() =>
+              signIn('github', {
+                callbackUrl: `${window.location.origin}/`,
+              })
+            }
+          >
+            <img src="assets/github.svg" alt="Github" />
+            Faça login com seu Github para começar
+          </button>
+        </main>
+      </div>
+    </div>
+  )
+}
